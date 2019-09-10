f61b0fc71a4283383f7a570c371769c7404a8812
Mwamodo Erick <mwamodojnr@gmail.com>
2019-09-10 14:01:28 +0300

    WIP

---
+++ src/components/Todos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <!-- <h3>{{ todo.title }}</h3> -->
+      <TodoItem v-bind:todo="todo" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from "@/components/TodoItem";
+
+export default {
+  name: "Todos",
+  props: ["todos"],
+  components: {
+    TodoItem
+  }
+};
+</script>
+
+<style scoped>
+</style>
